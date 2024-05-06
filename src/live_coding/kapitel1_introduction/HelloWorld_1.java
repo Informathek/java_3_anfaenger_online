9b388e60345f8d7c970e2e3ae09327d992b30150
Mohammad <orabe.mhd@gmail.com>
2024-05-06 12:45:27 +0200

    add other materials and init code

---
+++ src/live_coding/kapitel1_introduction/HelloWorld_1.java
@@ -0,0 +1,21 @@
+package live_coding.kapitel1_introduction;
+
+class HelloWorld_1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello und willkomen zu meinem Programm!");
+        System.out.println("----------------");
+
+        System.out.println("Hier ist mein username:");
+        String username = "Moh";
+        System.out.println(username);
+
+        System.out.println("Hier ist mein password:");
+        String password = "23459SDF@_.1";
+        System.out.println(password);
+
+        System.out.println("Ende des Programms");
+        System.out.println("----------------");
+
+    }
+}
